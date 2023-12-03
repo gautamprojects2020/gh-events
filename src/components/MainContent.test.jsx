1cc39c5988697f06466f362752fe060a1612ca4d
GAUTAM <gautamgovinda38@gmail.com>
2023-12-03 18:35:50 +0530

    another added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 // Added comment
+// Yet another comment
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
